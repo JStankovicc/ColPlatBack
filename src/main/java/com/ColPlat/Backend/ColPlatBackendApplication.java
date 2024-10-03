aa35e50499682ccf14c45b63b6d4372981196f49
Jovan Stankovic <j.stankovic001@gmail.com>
2024-10-03 17:25:59 +0200

    Created Spring Boot project.

---
+++ src/main/java/com/ColPlat/Backend/ColPlatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ColPlat.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColPlatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColPlatBackendApplication.class, args);
+	}
+
+}
